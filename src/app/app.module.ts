fd616322447170b5cf13340ab50923d240717eef
adriano-gil <adrianoagiljr@gmail.com>
2024-10-21 12:56:49 -0300

    buzzfeed project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizzComponent,
+    HomeComponent,
+    FooterBarComponent
   ],
   imports: [
     BrowserModule,
